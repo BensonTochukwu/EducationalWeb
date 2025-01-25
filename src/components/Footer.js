79449b5a7da201a3738476fbee220087aad51f54
BensonTochukwu <tecotochukwu@gmail.com>
2025-01-25 11:18:57 +0100

    Updated footer styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,10 +9,8 @@ const Footer = () => {
         
         <div className="footer-links">
           <a href="#about">About</a>
-          <a href="#learn-more">Learn More</a>
-          <a href="#contact">Contact Us</a>
+          <a href="#contact">Contact</a>
           <a href="#settings">Settings</a>
-          <a href="#terms">Terms of Use</a>
         </div>
       </div>
     </footer>
